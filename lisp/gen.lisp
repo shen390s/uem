8304df2b17c2ee6d0e9df1c76ea0bafaf0db7ae4
Rongsong Shen <rongsong.shen@icloud.com>
2024-11-08 15:35:48 +0800

    improve

--- lisp/gen.lisp
+++ lisp/gen.lisp
@@ -15,27 +15,31 @@
                           #'(lambda (filename)
                               (do-load filename))))
 
-(defun gen (module-path verbose output)
+(defun gen (module-config module-path verbose output)
   (format t "Generating configuration...~%")
-  (format t "load system from ~A~%" module-path)
+  (format t "load system from ~A~%" module-config)
+  (format t "package path:~a~%" module-path)
   (format t "Verbose: ~A~%" verbose)
   (in-package :uem)
   (let ((*readtable* (copy-readtable nil)))
     (set-dispatch-macro-character #\# #\/ #'read-doc-here)
     (format t "macro dispatch: ~a~%"
             (get-dispatch-macro-character #\# #\/))
-    (setf module-path (uiop/filesystem:truename* module-path))
-    (setf *uem-module-root*
-          (make-pathname :name nil
-                         :type nil
-                         :defaults module-path))
+    (setf module-config (uiop/filesystem:truename* module-config))
+    (if module-path
+	(setf module-path (uiop/filesystem:truename* module-path))
+	(setf module-path
+              (make-pathname :name nil
+                             :type nil
+                             :defaults module-config)))
+    (setf *uem-module-root* module-path)
     (let ((pkg-path (make-pathname :name "packages"
                                    :type nil
                                    :defaults module-path)))
       (progn
         (format t "Loading packages from ~a...~%" pkg-path)
 	(load-modules pkg-path)
-	(load module-path :verbose t :print t)
+	(load module-config :verbose t :print t)
         (let ((v *uem-sys*))
           (with-open-file (out (ensure-directories-exist output) :direction :output
 								 :if-exists :supersede
