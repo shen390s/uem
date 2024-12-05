9ebba6043d80eb51e0ed9ab64e0781744cde9b8c
Rongsong Shen <rongsong.shen@icloud.com>
2024-12-05 17:14:52 +0800

    make straight handle autoload of capnp

--- modules/emacs/packages/modes/capnp.lisp
+++ modules/emacs/packages/modes/capnp.lisp
@@ -2,4 +2,5 @@
        "Emacs mode to edit capnp definition"
        ("(capnp-mode :type git
 		      :host github
-		      :repo \"shen390s/capnp-mode\")"))
+		      :repo \"shen390s/capnp-mode\"
+                      :files (\"capnp-mode-pkg.el\" \"capnp-mode.el\"))"))
