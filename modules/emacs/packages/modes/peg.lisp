75102ec48995180d7b948367b42e55d6d0506f62
Rongsong Shen <rshen@shenrs.eu>
2026-03-16 11:04:05 +0800

    save

--- modules/emacs/packages/modes/peg.lisp
+++ modules/emacs/packages/modes/peg.lisp
@@ -1,6 +1,5 @@
-()
 (mode! peg
        "Emacs mode to edit peg file"
        ("(peg-mode :type git
 		      :host github
-		      :repo \"mattias-lundell/peg-mode\")"))
+		      :repo \"shen390s/peg-mode\")"))
