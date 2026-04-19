fb0924cab28ddd8accf96332b93feedd61302d00
Rongsong Shen <rshen@shenrs.eu>
2026-04-19 20:41:08 +0800

    add emacs claude

--- modules/emacs/packages/app/claude-code.lisp
+++ modules/emacs/packages/app/claude-code.lisp
@@ -1,30 +1,15 @@
 (defun claude-code-entry (self action )
   (case action
     ((:INIT) #/(progn
-                 (pkginstall '(inheritenv :type git :host github
-                                     :repo "purcell/inheritenv"))
-                 (pkginstall '(emacs-eat :type git
-                                     :host codeberg
-                                     :repo "akib/emacs-eat"
-                                     :files ("*.el" ("term" "term/*.el") "*.texi"
-                                             "*.ti" ("terminfo/e" "terminfo/e/*")
-                                             ("terminfo/65" "terminfo/65/*")
-                                             ("integration" "integration/*")
-                                             (:exclude ".dir-locals.el" "*-tests.el"))))
                  (pkginstall 'vterm)
-                 (pkginstall '(claude-code :type git :host github
-                                     :repo "stevemolitor/claude-code.el"
-                                     :branch "main"
-                                     :depth 1
-                                     :files ("*.el" (:exclude "images/*"))))
-                 (pkginstall '(monet :type git :host github
-                                     :repo "stevemolitor/monet"))
-                 (add-hook 'claude-code-process-environment-functions #'monet-start-server-function))
+                 (pkginstall '(claude-code-ide :type git
+                                               :host github
+                                               :repo "manzaltu/claude-code-ide.el")))
      /#
      )
     ((:CALL) #/(progn
-                 (monet-mode 1)
-                 (claude-code-mode))
+                 (setq claude-code-ide-terminal-backend 'vterm)
+                 (claude-code-ide-emacs-tools-setup))
      /#
      )
     (otherwise "")))
